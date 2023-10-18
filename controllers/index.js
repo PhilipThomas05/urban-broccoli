678b201c0f01343d78af8af0f9cb875bd49061bc
Mathew Poppell <mpoppell1002@gmail.com>
2023-10-18 14:23:25 -0400

    still working on routes.  login/logout

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const apiRoutes = require('../api');
+const homepageRoutes = require('../homepage-routes'); 
+
+router.use('/', homepageRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
