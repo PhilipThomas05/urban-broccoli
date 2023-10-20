61f6adf720e6aa70e60564eee1d960cb56122080
Mathew Poppell <mpoppell1002@gmail.com>
2023-10-20 11:23:45 -0400

    find-by get routes, first pass, not tested yet

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-logs-routes');
 
+const findbyRoutes = require('./findby-routes');
+
 router.use('/users', userRoutes);
+router.use('/search', findbyRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 
-const apiRoutes = require('../api');
-const homepageRoutes = require('../homepage-routes'); 
-
+const apiRoutes = require('./api');
+const homepageRoutes = require('./homepage-routes'); 
 router.use('/', homepageRoutes);
 router.use('/api', apiRoutes);
 
