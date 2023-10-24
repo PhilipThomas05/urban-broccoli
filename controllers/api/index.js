2dd83596370cbe62b28f2dd2ace38445365c066e
hana reiko <hanareiko@gmail.com>
2023-10-23 20:24:45 -0400

    listing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
+const apiRoutes = require('./api');
 const userRoutes = require('./user-logs-routes');
-
 const findbyRoutes = require('./findby-routes');
 
 router.use('/users', userRoutes);
 router.use('/search', findbyRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
